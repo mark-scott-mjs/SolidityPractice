cc8e772a905bd2c842cc0f60018cc0fba5beb908
Mark J. Scott <mark.scott.mjs@gmail.com>
2020-09-16 22:49:53 -0500

    payForToken.sol

---
+++ payForToken.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.1;
+
+contract payForToken {
+    mapping(address => uint256) public balances;
+    address payable wallet;
+    
+    event Purchase(address indexed _buyer, uint256 _amount);
+    
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
+    
+    function() external payable {
+        buyToken();
+    }
+    
+    function buyToken() public payable {
+        //buy a token
+        balances[msg.sender] += 1;
+        //send eth to the wallet 
+        wallet.transfer(msg.value);
+        emit Purchase(msg.sender,1);
+    }
+}
